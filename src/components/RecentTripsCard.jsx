64d2e6b71487d5facb7a2e9774f5fc55b9e073f1
Dawid <dawid.oleksy94@gmail.com>
2024-10-05 07:35:32 +0200

    hover img button

--- src/components/RecentTripsCard.jsx
+++ src/components/RecentTripsCard.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function RecentTripsCard({img, title, desc}) {
  return(
   <div className="recent-trips-card">
-    <img src={img} alt={title}/>
+    <div className="img-container">
+      <img src={img} alt={title}/>
+    </div>
     <h1>{title}</h1>
     <p>{desc}</p>
   </div>
